c15f873fd5477bacb92edf7e6579a6a8264e3de6
Ibee1 <muazxam@gmail.com>
2022-10-21 21:03:36 +0000

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_most_numbers - prints number from 0 to 9
+ * print_numbers - prints number from 0 to 9
  * 2 nad 4 not allowed
  * Return: void
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	char c;
 
