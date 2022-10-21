d5e80ccb3ed74e809b39e6beee3f60a9460bd221
Ibee1 <muazxam@gmail.com>
2022-10-21 20:59:13 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints number from 0 to 9
+ * 2 nad 4 not allowed
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar ('\n');
+}
