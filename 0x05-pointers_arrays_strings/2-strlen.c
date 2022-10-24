e044f8715d09407b1430714774238162674a59d3
Ibee1 <muazxam@gmail.com>
2022-10-24 22:31:42 +0000

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,12 +8,12 @@
  */
 int _strlen(char *s)
 {
-	int length = 0;
+	int count = 0;
 
-	while (s[length] !'\0')
+	while (*(s + count) != '\0')
 	{
-		length++;
+		count++;
 	}
-
-	return (length);
+	return (count);
 }
+
